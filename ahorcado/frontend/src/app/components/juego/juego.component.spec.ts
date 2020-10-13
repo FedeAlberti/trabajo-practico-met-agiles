41d36f9467828cca1757a4ef9bb9514491ec8111
facundo moreyra <sebastian.j.moreyra@gmail.com>
2020-10-13 19:05:27 -0300

    arriesgar letra

--- ahorcado/frontend/src/app/components/juego/juego.component.spec.ts
+++ ahorcado/frontend/src/app/components/juego/juego.component.spec.ts
@@ -1,4 +1,6 @@
+import { calcPossibleSecurityContexts } from '@angular/compiler/src/template_parser/binding_parser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Validators } from '@angular/forms';
 
 import { JuegoComponent } from './juego.component';
 
@@ -25,12 +27,12 @@ describe('JuegoComponent', () => {
 
   //Test para comprobar si se crea una palabra a adivinar.
   it('debería contener palabra a adivinar', () => {
-    expect(component.palabraAdivinar).toBeTruthy();
+    expect(component.palabraAdivinar).toEqual('hola');
   });
 
   it('debería contener palabra oculta', () => {
     component.generarPalabraOculta();
-    expect(component.palabraOculta).toBeTruthy();
+    expect(component.palabraOculta).toEqual('----');
   });
 
   it('debería ingresar una letra',() => {
@@ -41,6 +43,32 @@ describe('JuegoComponent', () => {
     expect(bandera).toBeTruthy();
 
 
+  })
+
+  it('deberia agregar la letra valida a la palabra oculta',() => {
+    let letra = 'a';
+    component.checkLetra(letra);
+    expect(component.palabraOculta).toContain(letra);
+  })
+
+  it('deberia sumar 1 a la cantidad de fallos',() => {
+    let letra = 'j';
+    let fallosAnterior = component.fallos; 
+    component.checkLetra(letra);
+    let fallosActuales = component.fallos;
+    expect(fallosActuales).toEqual(1); 
+  })
+
+  it('deberia ganar partida',() => {
+    component.palabraOculta = component.palabraAdivinar;
+    component.checkResultado();
+    expect(component.resultado).toEqual('Win'); 
+  })
+
+  it('deberia perder partida',() => {
+    component.fallos = 6;
+    component.checkResultado();
+    expect(component.resultado).toEqual('Lose'); 
   })
 
 });
