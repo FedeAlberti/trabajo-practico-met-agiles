e542d22f2eeef785acfea375abfce8900b63619f
Sebastian Moreyra <SebastianJMoreyra@hotmail.com>
2020-12-03 12:00:41 -0300

    add Ranking por dificultad option in navbar

--- ahorcado/frontend/src/app/components/nav-bar/nav-bar/nav-bar.component.ts
+++ ahorcado/frontend/src/app/components/nav-bar/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var M: any;
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  elems : any;
+  instances : any;
+  options = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.elems = document.querySelectorAll('.dropdown-trigger');
+    this.instances = M.Dropdown.init(this.elems, this.options);
   }
 
 }
