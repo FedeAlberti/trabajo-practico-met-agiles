0b8179071c04f401314c3cb8f9cc9b16b2814181
Sebastian Moreyra <SebastianJMoreyra@hotmail.com>
2020-12-04 18:58:32 -0300

    add harcoded tabla-puntaje

--- ahorcado/frontend/src/app/app-routing.module.ts
+++ ahorcado/frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { InicioComponent } from './components/inicio/inicio/inicio.component';
 import { JuegoComponent } from './components/juego/juego.component';
 import { NicknameFormComponent } from './components/nickname-form/nickname-form/nickname-form.component';
+import { TablaPuntajesComponent } from './components/tabla-puntajes/tabla-puntajes.component'
 
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'juego/:dificultad', component: JuegoComponent},
   { path: 'nickname', component: NicknameFormComponent},
+  { path: 'puntajes', component: TablaPuntajesComponent},
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: '**', redirectTo: 'inicio', pathMatch: 'full' },  
 ];
