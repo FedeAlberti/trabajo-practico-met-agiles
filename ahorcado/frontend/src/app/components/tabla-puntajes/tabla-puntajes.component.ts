12652eba9f8a3f645493dc8193460cddefa71b39
Sebastian Moreyra <SebastianJMoreyra@hotmail.com>
2020-12-04 20:07:50 -0300

    add tabla-puntajes functionality

--- ahorcado/frontend/src/app/components/tabla-puntajes/tabla-puntajes.component.ts
+++ ahorcado/frontend/src/app/components/tabla-puntajes/tabla-puntajes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-tabla-puntajes',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaPuntajesComponent implements OnInit {
 
-  constructor() { }
+  puntajes = [];
+
+  constructor(private gameService:GameService) { }
 
   ngOnInit(): void {
+    this.gameService.getPuntajeActual();
+    this.puntajes = this.gameService.puntajes;
+  }
+
+  validarIntentos(p){
+    if(p.aciertos + p.fallos !== 0) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  calcularPromedio(p) {
+    return ((p.aciertos)/(p.aciertos + p.fallos) * 100)
+  }
+
+  validarColor(p) {
+    let promedio = this.calcularPromedio(p);
+    console.log(promedio);
+    if( promedio <= 40) {
+      return "red"
+    }
+    else if (promedio <= 70) {
+      return 'yellow'
+    } 
+    else {
+      return 'green'
+    }
+
   }
 
 }
